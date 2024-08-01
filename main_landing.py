8715409de70d29bcb733361540e66e66dceb5d44
YU <lunasolyulika@mgmail.com>
2024-08-01 17:41:17 -0300

    adding change to upload to remmote

--- main_landing.py
+++ main_landing.py
@@ -2,3 +2,5 @@ print("inicio de landing")
 print("Muestro propaganda...")
 print("Intervencion de dev_login branch")
 print("Intervencion del main para ver en dev_login")
+
+print("MAIN: PUSHING CODE TO GITHUB")
